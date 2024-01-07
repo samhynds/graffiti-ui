35eb4564144b477cdc1eb9ad8a02498f8836a309
Sam Hynds <1131511+samhynds@users.noreply.github.com>
2024-01-07 15:18:35 +0000

    Add option for Label to be above or alongside its form input

--- src/Forms/Label/Label.tsx
+++ src/Forms/Label/Label.tsx
@@ -1,21 +1,35 @@
 import { HTMLAttributes } from "react";
 import styled from "styled-components";
 
-export interface LabelProps extends HTMLAttributes<HTMLLabelElement> {}
+export interface LabelProps extends HTMLAttributes<HTMLLabelElement> {
+  /**
+   * Should the label and input be above and below each other (column) or
+   * alongside each other (row)?
+   */
+  $layout?: "column" | "row";
+}
 
-const StyledLabel = styled.label`
+const StyledLabel = styled.label<LabelProps>`
   color: ${(props) => props.theme.colors.grey.xdark};
   font-size: 0.875rem;
   font-weight: 500;
   margin-bottom: 0.5rem;
   display: flex;
-  flex-direction: column;
+  flex-direction: ${(props) => props.$layout};
 `;
 
 /**
  * A label component which should be used alongside certain form inputs such as
  * TextInput, Dropdown, Checkbox, Radio and TextBox.
  */
-export const Label = ({ children, ...props }: LabelProps): JSX.Element => {
-  return <StyledLabel {...props}>{children}</StyledLabel>;
+export const Label = ({
+  children,
+  $layout = "column",
+  ...props
+}: LabelProps): JSX.Element => {
+  return (
+    <StyledLabel $layout={$layout} {...props}>
+      {children}
+    </StyledLabel>
+  );
 };
