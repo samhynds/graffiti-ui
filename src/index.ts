904453680a96ae07c99e2a76bfaae97cb98c86b8
Sam Hynds <1131511+samhynds@users.noreply.github.com>
2024-07-20 20:12:31 +0100

    Add build config and build files

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+// Themes and Styles
+import { ThemeProvider } from "styled-components";
+export { ThemeProvider };
+import { GlobalStyles } from "../src/GlobalStyles";
+export { GlobalStyles };
+import { defaultTheme } from "../src/themes/default";
+export { defaultTheme };
+
+// Buttons
+export * from "./Button/Standard";
+export * from "./Button/Icon";
+
+// Card
+export * from "./Card";
+
+// Form
+export * from "./Forms/Checkbox";
+export * from "./Forms/Dropdown";
+export * from "./Forms/Label";
+export * from "./Forms/Radio";
+export * from "./Forms/RadioGroup";
+export * from "./Forms/TextInput";
+export * from "./Forms/Wrapper";
+
+// Layout
+export * from "./Layout/Container";
+export * from "./Layout/Row";
+export * from "./Layout/Column";
+
+// Media
+export * from "./Media/ProfilePic";
+export * from "./Media/Video";
+
+// Modal
+export * from "./Modal";
+
+// Navigation
+export * from "./Navigation/Horizontal";
+export * from "./Navigation/Link";
+export * from "./Navigation/Vertical";
+
+// Overlay
+export * from "./Overlay";
+
+// Table
+export * from "./Table";
+
+// Tag
+export * from "./Tag";
+
+// Time
+export * from "./Time";
+
+// Toast
+export * from "./Toast";
+
+// Typography
+export * from "./Typography/Heading";
+export * from "./Typography/Text";
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.ts"),
+      },
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
